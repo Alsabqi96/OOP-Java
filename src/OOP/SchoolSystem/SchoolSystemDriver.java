6ce376e4dcb5e67c4f296287bdb2f128e9bf24f2
abdulmajeed <abdullmaged98916437>
2024-11-07 11:59:13 +0400

    Delete Spaces

--- src/OOP/SchoolSystem/SchoolSystemDriver.java
+++ src/OOP/SchoolSystem/SchoolSystemDriver.java
@@ -1,4 +1,5 @@
 package OOP.SchoolSystem;
+
 import OOP.SchoolSystem.Services.SchoolService;
 
 
@@ -7,24 +8,14 @@ import java.util.Scanner;
 public class SchoolSystemDriver {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter The School Details: ");
         SchoolService.addSchool();
 
 
-
-
     }
 
-
-
-
-
-
-
-
-    }
+}
 
 
 
